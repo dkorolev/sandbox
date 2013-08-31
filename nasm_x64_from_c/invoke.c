0874b50ec2f55b7cd9e54711baf9324ae78ce34b
Dima Korolev <dima@publicverification.com>
2013-08-31 16:33:02 -0700

    Extracting and returning parameters from const double x[2] argument.

--- nasm_x64_from_c/invoke.c
+++ nasm_x64_from_c/invoke.c
@@ -2,10 +2,13 @@
 #include <string.h>
 
 extern void foo(const char*, long long);
+extern double bar(const double*);
 
 int main() {
   const char* s = "Hehe! It works!\n";
+  const double x[2] = { 17, 42.042 };
   printf("Begin.\n");
   foo(s, strlen(s));
+  printf("%lf\n", bar(x));  // Prints x[1] == 42.042.
   printf("End.\n");
 }
--- nasm_x64_from_c/module.nasm
+++ nasm_x64_from_c/module.nasm
@@ -1,13 +1,17 @@
 [bits 64]
 
-global foo
+global foo, bar
 
 section .text
+
 foo:
 mov rax, 1
 mov rdx, rsi
 mov rsi, rdi
 mov rdi, 1
 syscall
+ret
 
+bar:
+movq xmm0, [rdi+8]  ; extracts x[1] when presented an array of doubles.
 ret
