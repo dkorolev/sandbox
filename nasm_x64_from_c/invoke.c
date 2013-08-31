278aeffa4ca46d202a0b59011dc3a22fa95466f6
Dima Korolev <dima@publicverification.com>
2013-08-31 15:35:37 -0700

    Discovered what seems to be a stack corruption problem.

--- nasm_x64_from_c/invoke.c
+++ nasm_x64_from_c/invoke.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
 #include <string.h>
 
 extern void foo(const char*, long long);
 
 int main() {
   const char* s = "Hehe! It works!\n";
+  printf("Begin.\n");
   foo(s, strlen(s));
+  printf("End.\n");
 }
