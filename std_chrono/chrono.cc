6ef7ecdd9b2ecc95b9806e22246ed18da8dba12e
Dima Korolev <dima@publicverification.com>
2013-09-01 17:13:10 -0700

    Tested fixed time usecase.

--- std_chrono/chrono.cc
+++ std_chrono/chrono.cc
@@ -4,23 +4,43 @@
 #include <chrono>
 #include <vector>
 
-void long_function() {
+void test1() {
+  std::chrono::high_resolution_clock timer;
   std::vector<long> x;
+
+  auto start_time = timer.now();
+
   x.push_back(1);
   x.push_back(1);
   for(int i = 2; i < 10000000; i++) {
     x.push_back(x[i-2] + x[i-1]);
   }
+
+  auto end_time = timer.now();
+
+  std::cout << "It took " << std::chrono::duration_cast<std::chrono::duration<int, std::milli>>(end_time - start_time).count() << "ms " << std::endl;
+}
+
+void test2() {
+  std::chrono::high_resolution_clock timer;
+  std::vector<long> x;
+
+  auto start_time = timer.now();
+
+  x.push_back(1);
+  x.push_back(1);
+
+  double duration;
+  do {
+    size_t i = x.size();
+    x.push_back(x[i-2] + x[i-1]);
+    duration = std::chrono::duration_cast<std::chrono::duration<int, std::milli>>(timer.now() - start_time).count();
+  } while (duration < 875);
+
+  std::cout << duration << "ms, " << x.size() << " numbers." << std::endl;
 }
 
 int main() {
-  std::chrono::high_resolution_clock timer;
-
-  auto start_time = timer.now();
-  long_function();
-  auto end_time = timer.now();
-
-  std::cout << "It took " <<
-  std::chrono::duration_cast<std::chrono::duration<int, std::milli>>(end_time - start_time).count() << "ms " << std::endl;
-  return 0;
+  test1();
+  test2();
 }
